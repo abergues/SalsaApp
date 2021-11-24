4eccc3250aa55ca9593ea34b71e6369e3e14161a
eeroolli <eero.olli@gmail.com>
2021-11-24 16:25:53 +0100

    Creating the files at DSR

---
+++ find_the_beat.py
@@ -0,0 +1,28 @@
+
+"""
+# Use time series to find the beat in music
+The beat should be used to select images and keypoints for notation
+
+
+1. Read the csv file with info.
+2. Convert to timeseries
+3. find the length of cycle
+4. select the 0 movement point for the feet, as the "Beat"
+5. Mark this in the data
+6. save csv data
+
+"""
+
+#### AUTHENTICATE USER/APP on Google
+import pandas as pd
+import requests
+import csv
+from pathlib import Path
+import os
+from os.path import exists, join, basename, splitext
+
+
+# https://drive.google.com/file/d/12v8kJJp_Bvsz3xJMc3olvl-3Wce3_Acb/view?usp=sharing
+url = requests.get('https://drive.google.com/file/d/12v8kJJp_Bvsz3xJMc3olvl-3Wce3_Acb/view?usp=sharing')
+csv_raw = StringIO(url.text)
+dfs = pd.read_csv(csv_raw)
---
+++ identify_the_dancers.py
@@ -0,0 +1,5 @@
+
+
+"""
+use YOLO then add the ID to the openpose data
+"""
