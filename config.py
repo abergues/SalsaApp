4bdcd550bdad8fbde2d7f789cefd58d6d8af58f4
Eero Olli M720 <eero.olli@gmail.com>
2021-12-06 12:27:18 +0100

    initial commit of config.py and .gitignore

---
+++ config.py
@@ -0,0 +1,14 @@
+# Config for local use.
+root_path = "C:\Users\droll\Documents\DSR\DanceAppProject\SalsaApp"
+
+# requires that gdrive is mounted
+# root_path = "/gdrive/My Drive/DSR/DanceApp"
+
+#MY_FILE_PATH = "./abc/xyz.csv"
+
+# Google Drive root directory
+
+
+# Task_file >>
+# import config
+# pandas.read_csv(config.MY_FILE_PATH)
